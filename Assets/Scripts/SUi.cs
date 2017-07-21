81380433ab37ffc5a4750cdf215726f6f044048c
Lazyevilsquid <kisun0401@naver.com>
2017-07-21 18:33:57 +0900

    예찬:kiss:

--- Assets/Scripts/SUi.cs
+++ Assets/Scripts/SUi.cs
@@ -7,7 +7,11 @@ public class SUi : MonoBehaviour
     [SerializeField]
     UnityEngine.UI.Text timer;
     [SerializeField]
+    UnityEngine.UI.Text skill;
+    [SerializeField]
     GameObject BtnGame = null;
+    [SerializeField]
+    GameObject ResultGame = null;
   
     void Start()
     {
@@ -16,16 +20,27 @@ public class SUi : MonoBehaviour
     
     void Update()
     {
+        if (SGameMng.I.bStartCheck)
+        {
+            if (timer.text.Equals("0:00")) Result();
+            else if (SGameMng.I.thiefCount < 0) Result();
+        }
+
         timer.text = SGameMng.I.sTimer;
         if (timer.text.Equals("1:0"))
             timer.color = new Color(255, 191, 0);
         else if (timer.text.Equals("0:30"))
             timer.color = new Color(255, 69, 83);
         else if (timer.text.Equals(""))
-            timer.color = new Color(255, 54, 0);
+            timer.color = new Color(255, 255, 255);
 
         if (GM.NetworkManager.getInstance.isAdmin && !SGameMng.I.bStartCheck)
             BtnGame.SetActive(true);
+    }
+
+    void Result()
+    {
+        ResultGame.SetActive(true);
     }
 
     public void gameStart()
