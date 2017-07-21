cdb13de9a8d5c1be35b1bb0fffd1a3073a5e3128
Lazyevilsquid <kisun0401@naver.com>
2017-07-21 21:42:07 +0900

    reset:trollface:

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reset : MonoBehaviour
+{
+    public SPlayerMove playerScrp;
+
+    public void ResetFn()
+    {
+        playerScrp.isLive = true;
+        playerScrp.nhp = 1;
+        playerScrp.pos = new Vector2(0f, 0f);
+        playerScrp.bBlind = false;
+        playerScrp.fSpeed = 9f;
+        playerScrp.SkillGame.SetActive(false);
+        playerScrp.proper = PROPER.GENERAL;
+        playerScrp.color = COLOR.WHITE;
+    }
+}
