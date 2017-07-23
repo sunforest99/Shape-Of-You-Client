3c1889102521af415314defa03f20407b3c68f98
kyechan99 <kyechan99@naver.com>
2017-07-23 12:44:41 +0900

    improve ui

--- Assets/Scripts/User/Col.cs
+++ Assets/Scripts/User/Col.cs
@@ -5,12 +5,7 @@ using UnityEngine;
 public class Col : MonoBehaviour
 {
     public SPlayerMove playerScrp = null;
-    [SerializeField]
-    SpriteRenderer colColor = null;
-
-    public void SetColor() { colColor.color = new Color(0.69f, 0.56f, 0.51f, 1f); }
-    public void ColorReset() { colColor.color = new Color(1f, 1f, 1f, 0f); }
-
+    
     void OnTriggerEnter2D(Collider2D col)
     {
         //if (playerScrp.isPlayer)
